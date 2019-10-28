f6febb295877c220adc0d9299eb254ff134cb95a
DaniAkash <s.daniakash@gmail.com>
2019-10-28 22:54:07 +0530

    removed temp redirection

--- src/pages/index.js
+++ src/pages/index.js
@@ -8,15 +8,6 @@ import { rhythm } from "../utils/typography"
 
 class BlogIndex extends React.Component {
 
-  /**
-   * Temporary redirection, should be removed
-   */
-  componentDidMount() {
-    if(window.location.href.includes("daniakash.github.io")) {
-      window.location.href = "https://science-and-stardust.xyz";
-    }
-  }
-
   render() {
     const { data } = this.props
     const siteTitle = data.site.siteMetadata.title
